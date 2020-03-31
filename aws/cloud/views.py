6d21ac5b6a874dcc296f2766aa951927be32f0d4
KaranKathiriya <karankathiriya@gmail.com>
2020-03-31 12:30:19 +0530

    Complete Project

--- aws/cloud/views.py
+++ aws/cloud/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 import cv2
 import boto3
 from botocore.exceptions import NoCredentialsError
 import os
-#ACCESS_KEY = 'AKIAWZWHC4N22ZGNBSZ3'
-#SECRET_KEY = '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'
+from django.contrib import messages
+
+ACCESS_KEY = ''
+SECRET_KEY = ''
 count = 0
+flag = 0
 
 
 def home(request):
@@ -19,85 +22,79 @@ def capture_img(request):
     global count
     global ACCESS_KEY
     global SECRET_KEY
-    if request.method == 'POST':
-        name = request.POST['name']
+    global flag
+    if request.method == 'POST' and flag == 0:
+        flag = 1
+        name = request.POST['filename']
         filename = name + ".jpg"
         ACCESS_KEY = request.POST['ak']
         SECRET_KEY = request.POST['sk']
-    else:
-        count = count + 1
-        filename = "cloud" + str(count) + ".jpg"
-    key = cv2.waitKey(1)
-    webcam = cv2.VideoCapture(0)
-    while True:
-        try:
-            check, frame = webcam.read()
-            print(check)  # prints true as long as the webcam is running
-            print(frame)  # prints matrix values of each framecd
-            cv2.imshow("Capturing", frame)
+        bucket = request.POST['bk']
+        if ACCESS_KEY != "" and SECRET_KEY != "" and bucket != "" and filename != "":
             key = cv2.waitKey(1)
-            if key == ord('s'):
-                cv2.imwrite(filename, img=frame)
-                webcam.release()
-                img_new = cv2.imread(filename)
-                img_new = cv2.imshow("Captured Image", img_new)
-                cv2.waitKey(1650)
-                cv2.destroyAllWindows()
+            webcam = cv2.VideoCapture(0)
+            while True:
+                try:
+                    check, frame = webcam.read()
+                    print(check)  # prints true as long as the webcam is running
+                    print(frame)  # prints matrix values of each framecd
+                    cv2.imshow("Capturing", frame)
+                    key = cv2.waitKey(1)
+                    if key == ord('s'):
+                        cv2.imwrite(filename, img=frame)
+                        webcam.release()
+                        img_new = cv2.imread(filename)
+                        img_new = cv2.imshow("Captured Image", img_new)
+                        cv2.waitKey(1650)
+                        cv2.destroyAllWindows()
 
-                break
-            elif key == ord('q'):
-                print("Turning off camera.")
-                webcam.release()
-                print("Camera off.")
-                print("Program ended.")
-                cv2.destroyAllWindows()
-                break
+                        break
+                    elif key == ord('q'):
+                        print("Turning off camera.")
+                        webcam.release()
+                        print("Camera off.")
+                        print("Program ended.")
+                        cv2.destroyAllWindows()
+                        break
 
-        except(KeyboardInterrupt):
-            print("Turning off camera.")
-            webcam.release()
-            print("Camera off.")
-            print("Program ended.")
-            cv2.destroyAllWindows()
-            break
-    uploaded = upload_img(filename)
-    print(uploaded)
-    if uploaded == True:
-        return HttpResponse("<h1>Image Uploaded Successfully</h1>")
+                except(KeyboardInterrupt):
+                    print("Turning off camera.")
+                    webcam.release()
+                    print("Camera off.")
+                    print("Program ended.")
+                    cv2.destroyAllWindows()
+                    break
+            local_file = filename
+            #bucket = bucket_name
+            s3_file = filename
+            s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                              aws_secret_access_key=SECRET_KEY)
+
+            try:
+                s3.upload_file(local_file, bucket, s3_file)
+                print("Upload Successful")
+                os.remove(local_file)
+                messages.info(request, 'Image Uploaded successfully')
+            except FileNotFoundError:
+                print("The file was not found")
+                messages.info(request, 'Please Capture an Image to upload')
+                # return render(request, 'index.html')
+            except NoCredentialsError:
+                print("Credentials not available")
+                os.remove(local_file)
+                messages.info(
+                    request, 'Please Check ACCESS KEY and SECRET KEY')
+                # return render(request, 'index.html')
+            except:
+                os.remove(local_file)
+                messages.info(
+                    request, 'Please Check ACCESS KEY and SECRET KEY')
+
+            filename = ""
+            ACCESS_KEY = ""
+            SECRET_KEY = ""
+            bucket = ""
+            return render(request, 'index.html')
     else:
-        return HttpResponse("<h1>Please Check ACCESS KEY and SECRET KEY</h1>")
-
-
-def upload_img(filename):
-    local_file = filename
-    bucket = "cloud-a-thon"
-    s3_file = filename
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                      aws_secret_access_key=SECRET_KEY)
-
-    try:
-        s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
-        os.remove(local_file)
-        return True
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
-    except:
-        return False
-
-
-@csrf_exempt
-def save_image(request):
-    if request.POST:
-        # save it somewhere
-        f = open('/static/user/save_image/someimage.jpg', 'wb')
-        f.write(request.raw_post_data)
-        f.close()
-        # return the URL
-        return HttpResponse('http://localhost:8080/site_media/webcamimages/someimage.jpg')
-    else:
-        return HttpResponse('no data')
+        messages.info(request, 'Please fill all the details')
+        return render(request, 'index.html')
